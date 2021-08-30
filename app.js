ba865ed8aa00a5876c2ae336cbb4a2a5689c9a62
Davide Del Popolo <davidedp91@gmail.com>
2021-08-30 13:24:58 +0200

    add mongodb endpoint

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import DB from './config/db';
+import CORS from './middlewares/cors';
+import notfound from './middlewares/notfound';
+import error from './middlewares/error';
+import { restRouter } from './api';
 
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World.');
-});
+// crate database connection
+DB.connect();
+
+app.use(express.json());
+app.use(CORS.handleCors);
+app.use('/api', restRouter);
+app.use(notfound);
+app.use(error);
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT http://localhost:${PORT}`);
