7e3277c7baeb4a2e2acfb506c7d0c0088f52fa10
Davide Del Popolo <davidedp91@gmail.com>
2021-09-03 10:07:55 +0200

    added some console logging

--- api/resources/step/step.controller.js
+++ api/resources/step/step.controller.js
@@ -1,4 +1,5 @@
-import Step from './step.model'
+import Step from './step.model';
+import mongoose from 'mongoose';
 
 const EXISTS = 'EXISTS';
 const SUCCESS = 'SUCCESS';
@@ -8,6 +9,7 @@ const NOT_FOUND = 'NOT_FOUND';
 export default {
     // Create a step
     async createStep(req, res) {
+        console.log("Creating step: \n" + req.body.description);
 
         try {
 
@@ -20,6 +22,7 @@ export default {
                 return res.send({ message: EXISTS });
             }
         } catch (error) {
+            console.log(error);
             return res.status(500).send(error);
         }
     },
@@ -32,13 +35,17 @@ export default {
             const steps = await Step.find();
             return res.send({ message: SUCCESS, data: steps });
         } catch (error) {
-
             return res.status(500).send(error);
         }
     },
 
-    // Find step by number & 
+    // Find step by number 
     async findStep(req, res) {
+        let objects = "";
+        Object.keys(req.query).forEach(key => {
+            objects += key + ": " + req.query[key] + "\n";
+        })
+        // console.log("Finding steps: " + objects);
 
         try {
 
@@ -59,12 +66,12 @@ export default {
             if (description !== undefined) {
                 filter.description = description;
             }
-            if (module !== undefined){
+            if (module !== undefined) {
                 filter.module = module;
             }
 
             const foundStep = await Step.find(filter);
-            
+
             if (foundStep.length > 0) {
                 return res.send({ message: SUCCESS, data: foundStep });
             } else {
@@ -78,11 +85,12 @@ export default {
 
     // Update step
     async updateStep(req, res) {
+        console.log("Updating step: " + req.query.id);
 
         try {
 
             let step = {
-                id: req.body.id,
+                _id: mongoose.Types.ObjectId(req.body.id),
                 number: req.body.number,
                 description: req.body.description,
                 image: req.body.image,
@@ -99,17 +107,18 @@ export default {
                 return res.send({ message: FAILED });
             }
         } catch (error) {
-
+            console.log(error);
             return res.status(500).send(error);
         }
     },
 
     // Delete user by Id
     async deleteStep(req, res) {
-
+        console.log("Deleteing step: " + req.query.id);
         try {
-
-            const deletedStep = await Step.findByIdAndRemove(req.query.id)
+            const id = mongoose.Types.ObjectId(req.query.id);
+            console.log(id);
+            const deletedStep = await Step.findByIdAndRemove(id);
             if (deletedStep) {
 
                 return res.send({ message: SUCCESS });
@@ -119,7 +128,7 @@ export default {
             }
 
         } catch (error) {
-
+            console.log(error);
             return res.status(500).send(error);
         }
     }
